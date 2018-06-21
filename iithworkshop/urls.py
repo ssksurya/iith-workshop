1f1b60a903f730d218d1a45994fa2ae31c6c2bfc
avravikiran-sopho <me15btech11039@iith.ac.in>
2018-06-22 04:01:20 +0530

    404

--- iithworkshop/urls.py
+++ iithworkshop/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.views.static import serve
 from django.conf import settings
 
+
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('app.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
 ]
+
+handler404 = 'app.views.handler404'
+handler500 = 'app.views.handler500'
