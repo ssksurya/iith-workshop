4cd9efb1443f6c1848be66d1c6dfc9a40a053e62
avravikiran-sopho <me15btech11039@iith.ac.in>
2018-06-21 13:40:12 +0530

    to iith server

--- app/views.py
+++ app/views.py
@@ -427,6 +427,12 @@ def handler404(request):
     response.status_code = 404
     return response
 
+def handler500(request):
+    response = render_to_response('404.html', {},
+                              context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
+
 
 
 
